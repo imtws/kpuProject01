5ed4d04f09a972235a100d7d11ece31484dd96eb
dlwnsdud01 <nexus012345@naver.com>
2024-06-10 23:39:40 +0900

    Update result.js

--- static/result.js
+++ static/result.js
@@ -1,3 +1,4 @@
+// result.js
 document.addEventListener('DOMContentLoaded', function() {
     // Load data from session or API and display results
     fetch('/get_results')
@@ -7,10 +8,12 @@ document.addEventListener('DOMContentLoaded', function() {
                 const analysisResultDiv = document.getElementById('analysis-result');
                 const recommendationsDiv = document.getElementById('recommendations');
                 const plotImage = document.getElementById('plot-image');
+                const piePlotImage = document.getElementById('pie-plot-image');
 
                 analysisResultDiv.innerHTML = `<p>${data.result}</p>`;
                 recommendationsDiv.innerHTML = `<p>${data.recommendations}</p>`;
                 plotImage.src = data.plot_url;
+                piePlotImage.src = data.pie_plot_url;
             } else {
                 document.getElementById('result').classList.add('hidden');
                 document.getElementById('error-message').classList.remove('hidden');
