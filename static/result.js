9d05cbe62dab2600e5459637fb99d297fb90bac2
dlwnsdud01 <nexus012345@naver.com>
2024-06-12 21:50:46 +0900

    Update result.js

--- static/result.js
+++ static/result.js
@@ -4,19 +4,23 @@ document.addEventListener('DOMContentLoaded', function() {
     fetch('/get_results')
         .then(response => response.json())
         .then(data => {
-            if (data.success) {
-                const analysisResultDiv = document.getElementById('analysis-result');
-                const recommendationsDiv = document.getElementById('recommendations');
-                const plotImage = document.getElementById('plot-image');
-                const piePlotImage = document.getElementById('pie-plot-image');
+            const analysisResultDiv = document.getElementById('analysis-result');
+            const recommendationsDiv = document.getElementById('recommendations');
+            const plotImage = document.getElementById('plot-image');
+            const piePlotImage = document.getElementById('pie-plot-image');
+            const resultDiv = document.getElementById('result');
+            const errorMessageDiv = document.getElementById('error-message');
 
+            if (data.success) {
                 analysisResultDiv.innerHTML = `<p>${data.result}</p>`;
                 recommendationsDiv.innerHTML = `<p>${data.recommendations}</p>`;
                 plotImage.src = data.plot_url;
                 piePlotImage.src = data.pie_plot_url;
+                resultDiv.classList.remove('hidden');
+                errorMessageDiv.classList.add('hidden');
             } else {
-                document.getElementById('result').classList.add('hidden');
-                document.getElementById('error-message').classList.remove('hidden');
+                resultDiv.classList.add('hidden');
+                errorMessageDiv.classList.remove('hidden');
             }
         })
         .catch(error => {
